7f36b385a83f5448f993fa0d13715836ce6b3c75
Nathan Lui <nathanlui2010@gmail.com>
2019-12-03 16:11:24 -0800

    Averaging data from BCI

--- python/main.py
+++ python/main.py
@@ -2,8 +2,28 @@ import asyncio
 import json
 from lib.cortex import Cortex
 
+def update_stats(stats, data, count):
+    new_count = count + 1
+    stats[0] = (stats[0] * count + data['met'][1]) / new_count
+    stats[1] = (stats[1] * count + data['met'][3]) / new_count
+    stats[2] = (stats[2] * count + data['met'][6]) / new_count
+    stats[3] = (stats[3] * count + data['met'][8]) / new_count
+    stats[4] = (stats[4] * count + data['met'][10]) / new_count
+    stats[5] = (stats[5] * count + data['met'][12]) / new_count
+    return new_count
 
-def load_cortex():
+def print_data(data):
+    print("Data:", data)
+    print("Met:", data['met'])
+    print("Engagement:", data["met"][1])
+    print("Excitement:", data["met"][3])
+    print("Long-term Excitement:", data["met"][4])
+    print("Stress:", data["met"][6])
+    print("Relaxation:", data["met"][8])
+    print("Interest:", data["met"][10])
+    print("Focus:", data["met"][12])
+
+async def connect(cortex):
     # await cortex.inspectApi()
     #print("** USER LOGIN **")
     await cortex.get_user_login()
@@ -19,32 +39,7 @@ def load_cortex():
     await cortex.get_license_info()
     #print("** QUERY HEADSETS **")
     await cortex.query_headsets()
-    return len(cortex.headsets) > 0
-    
-
-def update_stats(stats, data):
-    count += 1
-    stats[0] += data['met'][1]
-    stats[1] += data['met'][3]
-    stats[2] += data['met'][6]
-    stats[3] += data['met'][8]
-    stats[4] += data['met'][10]
-    stats[5] += data['met'][12]
-
-def print_data(data):
-    print("Data:", data)
-    print("Met:", data['met'])
-    print("Engagement:", data["met"][1])
-    print("Excitement:", data["met"][3])
-    print("Long-term Excitement:", data["met"][4])
-    print("Stress:", data["met"][6])
-    print("Relaxation:", data["met"][8])
-    print("Interest:", data["met"][10])
-    print("Focus:", data["met"][12])
-
-async def do_stuff(cortex):
-    stats = []
-    if load_cortex():
+    if len(cortex.headsets) > 0:
         #print("** CREATE SESSION **")
         await cortex.create_session(activate=True,
                                     headset_id=cortex.headsets[0])
@@ -52,16 +47,19 @@ async def do_stuff(cortex):
         await cortex.create_record(title="test record 1")
         #print("** SUBSCRIBE MET **")
         await cortex.subscribe(['met'])
+        
+        stats = [0, 0, 0, 0, 0, 0]
+        count = 0
         while input().strip() is not "q":
             data = json.loads(await cortex.get_data())
             print_data(data)
-            update_stats(stats, data)
+            count = update_stats(stats, data, count)
         await cortex.close_session()
 
 
 def run():
     cortex = Cortex('./cortex_creds')
-    asyncio.run(do_stuff(cortex))
+    asyncio.run(connect(cortex))
     cortex.close()
 
 
