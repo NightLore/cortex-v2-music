3e495d99b8b780da1aa595e53bb139b99422acae
Nathan Lui <nathanlui2010@gmail.com>
2019-12-10 13:20:34 -0800

    Added method to run program without headset

--- python/README.md
+++ python/README.md
@@ -5,5 +5,11 @@ python main.py
 ```
 in order to start MoodMusic recommendation services.
 
+Run
+```bash
+python example.py
+```
+in order to start MoodMusic recommendation services without the headset. This will essentially replace the headset with a random stats generator.
+
 The library of music used is in the music folder and will thus be based on what has been given to the program to recommend.
 
--- python/example.py
+++ python/example.py
@@ -1,44 +1,70 @@
-import asyncio
-from lib.cortex import Cortex
+import json
+import random
+import MoodMusic.skeleton as skeleton
+import music.importer as importer
 
+def update_stats(stats, data):
+    stats[0] = data['met'][1]
+    stats[1] = data['met'][3]
+    stats[2] = data['met'][6]
+    stats[3] = data['met'][8]
+    stats[4] = data['met'][10]
+    stats[5] = data['met'][12]
 
-async def do_stuff(cortex):
-    # await cortex.inspectApi()
-    print("** USER LOGIN **")
-    await cortex.get_user_login()
-    print("** GET CORTEX INFO **")
-    await cortex.get_cortex_info()
-    print("** HAS ACCESS RIGHT **")
-    await cortex.has_access_right()
-    print("** REQUEST ACCESS **")
-    await cortex.request_access()
-    print("** AUTHORIZE **")
-    await cortex.authorize(debit=10000)
-    print("** GET LICENSE INFO **")
-    await cortex.get_license_info()
-    print("** QUERY HEADSETS **")
-    await cortex.query_headsets()
-    if len(cortex.headsets) > 0:
-        print("** CREATE SESSION **")
-        await cortex.create_session(activate=True,
-                                    headset_id=cortex.headsets[0])
-        print("** CREATE RECORD **")
-        await cortex.create_record(title="test record 1")
-        print("** SUBSCRIBE POW & MET **")
-        await cortex.subscribe(['pow', 'met'])
-        while cortex.packet_count < 10:
-            await cortex.get_data()
-        await cortex.inject_marker(label='halfway', value=1,
-                                   time=cortex.to_epoch())
-        while cortex.packet_count < 20:
-            await cortex.get_data()
-        await cortex.close_session()
+def print_data(data):
+    print("Data:", data)
+    print("Met:", data['met'])
+    print("Engagement:", data["met"][1])
+    print("Excitement:", data["met"][3])
+    print("Long-term Excitement:", data["met"][4])
+    print("Stress:", data["met"][6])
+    print("Relaxation:", data["met"][8])
+    print("Interest:", data["met"][10])
+    print("Focus:", data["met"][12])
 
+def recommend(net, stats, songs, outputs):
+    feeling_index = net.get_output_index(stats)
+    feeling = outputs[feeling_index]
+    print("--------------------------------------------")
+    print("\nI believe you are feeling", feeling, flush=True)
+    recommendation = importer.getRandom(songs[feeling_index])
+    print("I recommend:", recommendation, flush=True)
+    return feeling
+
+def querySatisfaction(feeling):
+    satisfied = input("...\nDo you like your recommendation? (Yes/No) ").strip().lower()
+    if satisfied[0] is 'n':
+        return input("How are you really feeling? (Choose one)\n"
+                     "(Angry|Excited|Focused|Happy|Relaxed|Sad)|Pass|Quit\n").strip().lower()
+    if satisfied[0] is 'q':
+        return satisfied[0]
+    return feeling
+
+def get_random_stats():
+    return [random.random(),
+            random.random(),
+            random.random(),
+            random.random(),
+            random.random(),
+            random.random()]
 
 def test():
-    cortex = Cortex('./cortex_creds')
-    asyncio.run(do_stuff(cortex))
-    cortex.close()
+        
+    stats = [0, 0, 0, 0, 0, 0]
+    count = 0
+    outputs = skeleton.getDefaultOutputs()
+    net = skeleton.getNetwork()
+    songs = importer.importFiles()
+
+    stats = get_random_stats()
+    
+    feeling = querySatisfaction(recommend(net, stats, songs, outputs))
+    while feeling[0] is not 'q':
+        if feeling[0] is not 'p':
+            skeleton.superlearn(net, stats, skeleton.fixer(feeling, skeleton.getDefaultOutputs()))
+
+        stats = get_random_stats()
+        feeling = querySatisfaction(recommend(net, stats, songs, outputs))
 
 
 if __name__ == '__main__':
